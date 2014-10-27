eae8a86c38c22ed351d833c5be792cfeccee505b
MappingKat <katrina@engelsted.co>
2014-10-27 10:14:19 -0600

    deleting unnecessary folders

--- _site/includes/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$conf['app_name'] = "ModPods";
-	$conf['notify'] = "admin@modpod.com";
-	$conf['notification_from'] = "server@example.com";
-?>
--- _site/signup.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('includes/config.php');
-	include('includes/notification.php');
-	$notification = new Notification($conf['app_name'], $conf['notify'], $_POST['email'], $conf['notification_from']);
-	@header($notification->send());
-?>
